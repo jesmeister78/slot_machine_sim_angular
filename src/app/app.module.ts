8c6b7acde0aa2778e4ae27604f884306f23d1b30
Jesse Outh-Aut <Jesse.Outh-Aut@wolterskluwer.com>
2017-09-26 16:42:47 +1000

    getting the basic structure happening

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SymbolComponent } from './symbol.component';
+import { UserInterfaceComponent } from './user-interface.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SymbolComponent,
+    UserInterfaceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-interface.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-interface',
+    templateUrl: 'user-interface.component.html'
+})
+
+export class UserInterfaceComponent {
+}
